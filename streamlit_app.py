4979bc7667ae3513c47c9e44aae1d9b187f3dea9
subhapunati <subha_punati@yahoo.com>
2022-12-13 13:26:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,7 @@ streamlit.header ("Breakfast Menu")
 streamlit.text("Omega 3 & Bluberry oatmeal")
 streamlit.text("Hard boiled egg")
 streamlit.header ("Build your own smoothie")
+
+import pandas
+my_fruit_list=pandas.read.csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
