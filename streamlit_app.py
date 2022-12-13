ec0673162f1551552358800d4799a58c7615cb2a
subhapunati <subha_punati@yahoo.com>
2022-12-13 13:27:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,5 @@ streamlit.text("Hard boiled egg")
 streamlit.header ("Build your own smoothie")
 
 import pandas
-my_fruit_list=pandas.read.csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
